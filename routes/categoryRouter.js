7570c669e9fbda75af08eb903e418a31ca912972
sabbir-anwar <protik26@gmail.com>
2020-12-09 17:00:00 +0600

    deleteCategory || updateCategory route added

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -7,5 +7,9 @@ router.route('/category')
     .get(categoryCtrl.getCategories)
     .post(auth, authAdmin, categoryCtrl.createCategory)
 
+router.route('/category/:id')
+    .delete(auth, authAdmin, categoryCtrl.deleteCategory)
+    .put(auth, authAdmin, categoryCtrl.updateCategory)
+
 module.exports = router
 
