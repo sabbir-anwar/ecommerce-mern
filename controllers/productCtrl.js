19478b1583515530bfc4f8eed1030bb1abe6555c
sabbir-anwar <protik26@gmail.com>
2020-12-09 19:38:21 +0600

    productCtrl structure added

---
+++ controllers/productCtrl.js
@@ -0,0 +1,32 @@
+const Products = require('../models/productModel')
+
+const productCtrl = {
+  getProduct: async (req, res) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+  createProduct: async (req, res) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  },
+  updateProduct: async (req, res) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+}
