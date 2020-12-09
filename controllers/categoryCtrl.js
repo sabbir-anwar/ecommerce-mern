ac0b6dff0047b4bf46fbfe526839b3ac4ac5a20b
sabbir-anwar <protik26@gmail.com>
2020-12-09 16:59:10 +0600

    deleteCategory || updateCategory added

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -22,6 +22,24 @@ const categoryCtrl = {
     } catch (err) {
       return res.status(500).json({msg: err.message})
     }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      await Category.findByIdAndDelete(req.params.id)
+      res.json({msg: "Deleted a Category"})
+    } catch (err) {
+      return res.status(500).json({msg: err.message})
+    }
+  },
+  updateCategory: async (req, res) => {
+    try {
+      const {name} = req.body;
+      await Category.findOneAndUpdate({_id: req.params.id}, {name})
+
+      res.json({msg: "Updated successfully"})
+    } catch (err) {
+      return res.status(500).json({msg: err.message})
+    }
   }
 }
 
