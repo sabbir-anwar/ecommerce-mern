387090e55b8d942119c0a0067fe779f09e427b4a
sabbir-anwar <protik26@gmail.com>
2020-12-17 18:12:58 +0600

    header | main page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./GlobalState";
+import Header from './components/headers/Header'
+import MainPages from './components/mainpages/Pages'
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <DataProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <MainPages />
+        </div>
+      </Router>
+    </DataProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/mainpages/Pages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pages() {
+  return (
+    <div>
+      Main Page Component
+    </div>
+  )
+}
+
+export default Pages
