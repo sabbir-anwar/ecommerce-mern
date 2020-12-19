1b1380a8b510b104e72c65da53d783fb04b92dcf
sabbir-anwar <protik26@gmail.com>
2020-12-19 17:52:18 +0600

    Header component

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { GlobalState } from "../../GlobalState";
+import Menu from "./icon/menu.svg";
+import Close from "./icon/close.svg";
+import Cart from "./icon/cart.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const value = useContext(GlobalState);
   return (
-    <div>
-      Header Component
-    </div>
-  )
+    <header>
+      <div className="menu">
+        <img src={Menu} alt="" width="30" />
+      </div>
+
+      <div className="logo">
+        <h1>
+          <Link to="/">Shop</Link>
+        </h1>
+      </div>
+
+      <ul>
+        <li><Link to='/'>Products</Link></li>
+        <li><Link to='/login'>Login/Register</Link></li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
