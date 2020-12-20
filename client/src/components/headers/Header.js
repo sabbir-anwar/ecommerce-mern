d21b23208ad765fe05ce995fc205f0922c76369e
sabbir-anwar <protik26@gmail.com>
2020-12-20 16:39:00 +0600

    header style added

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -22,6 +22,17 @@ function Header() {
       <ul>
         <li><Link to='/'>Products</Link></li>
         <li><Link to='/login'>Login/Register</Link></li>
+
+        <li>
+          <img src={Close} alt="" width="30" className="menu" />
+        </li>
+
+        <div className="cart-icon">
+          <span>0</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="30" />
+          </Link>
+        </div>
       </ul>
     </header>
   );
